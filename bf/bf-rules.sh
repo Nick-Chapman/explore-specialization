58a850269357622fb6a2375e688244eb53d6a609
Nick Chapman <nic@amadido.co.uk>
2025-11-14 10:19:29 +0000

    make use of $@ jenga syntax

--- bf/bf-rules.sh
+++ bf/bf-rules.sh
@@ -2,31 +2,33 @@
 
 for name in "$@"; do
 
+out='$@' # prevent $@ from being replaced here; allowing jenga to replace it when interpreting the rule commands
+
 cat << EOF
 
 ${name}.the_prog : progs/${name}.bf
-  echo -n 'let the_prog = "' >> ${name}.the_prog
-  cat ${name}.bf >> ${name}.the_prog
-  echo '"' >> ${name}.the_prog
+  echo -n 'let the_prog = "' >> $out
+  cat ${name}.bf >> $out
+  echo '"' >> $out
 
 orig_${name}.ml : prim.ml ${name}.the_prog interpreter.fun
-  echo 'let the_mode = "ORIGINAL"' >> orig_${name}.ml
-  cat prim.ml >> orig_${name}.ml
-  cat ${name}.the_prog >> orig_${name}.ml
-  cat interpreter.fun >> orig_${name}.ml
-  echo 'let () = main()' >> orig_${name}.ml
+  echo 'let the_mode = "ORIGINAL"' >> $out
+  cat prim.ml >> $out
+  cat ${name}.the_prog >> $out
+  cat interpreter.fun >> $out
+  echo 'let () = main()' >> $out
 
 norm_${name}.ml : barefun.exe prim.ml shim.ml ${name}.fun
   ./barefun.exe ${name}.fun -2 -compile -mapp 1 -mlam 1 -ppu > ~/norm.ml
-  cat prim.ml >> norm_${name}.ml
-  cat shim.ml >> norm_${name}.ml
-  echo 'let Unit =' >> norm_${name}.ml
-  cat ~/norm.ml >> norm_${name}.ml
+  cat prim.ml >> $out
+  cat shim.ml >> $out
+  echo 'let Unit =' >> $out
+  cat ~/norm.ml >> $out
 
 ${name}.fun : ${name}.the_prog interpreter.fun
-  echo 'let the_mode = "NORMALIZED"' >> ${name}.fun
-  cat ${name}.the_prog >> ${name}.fun
-  cat interpreter.fun >> ${name}.fun
+  echo 'let the_mode = "NORMALIZED"' >> $out
+  cat ${name}.the_prog >> $out
+  cat interpreter.fun >> $out
 
 EOF
 done
